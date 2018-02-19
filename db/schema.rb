380f1eb50962ddd9d664aa785a215c4cd6e534a2
Aneta <anetad199@gmail.com>
2018-02-19 23:09:52 +0100

    Add name to Supervisor and Employee models

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180219210047) do
+ActiveRecord::Schema.define(version: 20180219220621) do
 
   create_table "employees", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_employees_on_email", unique: true
   end
 
@@ -25,6 +26,7 @@ ActiveRecord::Schema.define(version: 20180219210047) do
     t.string "encrypted_password", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_supervisors_on_email", unique: true
   end
 
