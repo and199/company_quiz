4337cf848d4f3eb24daa3b6f83a8fd1c02938dbc
Aneta D <anetad199@gmail.com>
2018-02-24 02:24:29 +0100

    Add AJAX request

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,13 +1,19 @@
 class AnswersController < ApplicationController
   def update
-    answer = Answer.find_by(question_id: params[:id], employee_id: current_user.id)
-    employee_answer = Question.find(params[:id]).choices.find(params[:question][:choices]).content
-    answer.update_attribute('content', employee_answer)
-    session[:questions].delete(params[:id].to_i)
-    if session[:questions].blank?
-      redirect_to employee_path(current_employee)
+    binding.pry
+    if request.xhr?
+      binding.pry
     else
-      redirect_to question_path(Question.find(session[:questions].sample))
+      answer = Answer.find_by(question_id: params[:id], employee_id: current_user.id)
+      employee_answer = Question.find(params[:id]).choices.find(params[:question][:choices]).content
+      answer.update_attribute('content', employee_answer)
+      session[:questions].delete(params[:id].to_i)
+      if session[:questions].blank?
+        redirect_to employee_path(current_employee)
+      else
+        redirect_to question_path(Question.find(session[:questions].sample))
+      end
     end
+
   end
 end
