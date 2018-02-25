e21ccbf1dcb2fb8e6382399c9bf5fa086981ac3f
Aneta D <anetad199@gmail.com>
2018-02-25 01:53:09 +0100

    Clean turbolinks loading

--- Gemfile
+++ Gemfile
@@ -40,7 +40,6 @@ gem 'hirb'
 gem 'pry-rails'
 gem 'pry-byebug'
 gem 'jquery-rails'
-gem 'jquery-turbolinks'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,12 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
-
-
-//=require jquery
-//=require jquery_ujs
-//=require jquery.turbolinks
-
-//=require turbolinks
-
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
 //= require bootstrap
